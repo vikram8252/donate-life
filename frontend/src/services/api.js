d2459f5383c7b42aba0ce78299df52397ef1ea80
vikram kumar <vikram8252>
2026-05-08 22:36:31 +0530

    solved api error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://donate-backend-8xk6.onrender.com"
+  baseURL: "https://donate-backend-8xk6.onrender.com/api"
   // baseURL: "http://localhost:5000/api"
 });
 
